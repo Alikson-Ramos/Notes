96263804d21b127927d239f9ecf14c350dd1f8c7
alikson <alikson.ramos@gmail.com>
2025-03-06 19:39:26 -0300

    validação

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,29 @@ class AuthController extends Controller
         return view('login');
     }
 
-    public function loginSubmit(Request $request) {}
+    public function loginSubmit(Request $request)
+    {
+        $request->validate(
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            [
+                'text_username.required' => 'O username é obrigatório',
+                'text_username.email' => 'O username deve ser um email válido',
+
+                'text_password.required' => 'O password é obrigatório',
+                'text_password.min' => 'O password deve ter pelo menos :min caracteres',
+                'text_password.max' => 'O password deve ter pelo menos :max caracteres'
+
+            ]
+        );
+
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        echo 'OK!';
+    }
 
     public function logout()
     {
